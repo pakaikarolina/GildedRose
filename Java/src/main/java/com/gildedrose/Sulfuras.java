232273ea9b4f1063a26b7dc8b98d81b26630f8e9
karolina_pakai <pakaikarolina@gmail.com>
2023-07-13 22:14:05 +0200

    created items extending Item.java

---
+++ Java/src/main/java/com/gildedrose/Sulfuras.java
@@ -0,0 +1,18 @@
+package com.gildedrose;
+
+public class Sulfuras extends Item {
+
+
+    public Sulfuras(int sellIn, int quality) {
+        super("Sulfuras, Hand of Ragnaros", sellIn, quality);
+    }
+
+    @Override
+    public void updateItem() {
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
